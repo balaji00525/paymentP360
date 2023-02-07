916b676c7cadfbfe3261c2dbb85b2197e7c3d697
moni15sha <monishasridhar15@gmail.com>
2023-02-07 20:53:03 +0530

    payment

--- src/app/amounttopay/amounttopay.component.ts
+++ src/app/amounttopay/amounttopay.component.ts
@@ -15,6 +15,7 @@ export class AmounttopayComponent implements OnInit {
   myDate: any = new Date();
   amount: number
   paymentMode: string;
+  literals:any={};
   selectedPaymentMode:string
   constructor(private services: ApiService,
     private dService: DataService,
@@ -27,11 +28,13 @@ export class AmounttopayComponent implements OnInit {
       case "biller": {
 
         this.services.getBillerData().subscribe(data => this.data = data);
+        this.services.getBillerLiteralData().subscribe(data=>this.literals = data);
         break;
       }
       case "sender": {
 
         this.services.getSenderData().subscribe(data => this.data = data);
+        this.services.getSenderLiteralData().subscribe(data=>this.literals = data);
         this.selectedPaymentMode='Pay';
 
         break;
@@ -39,6 +42,7 @@ export class AmounttopayComponent implements OnInit {
       case "requester": {
 
         this.services.getRequesterData().subscribe(data => this.data = data);
+        this.services.getRequesterLiteralData().subscribe(data=>this.literals = data);
         this.selectedPaymentMode='Request';
       }
     }
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -10,4 +10,7 @@ export class ApiService {
   getBillerData() { return this.http.get("http://localhost:3000/biller") }
   getSenderData() { return this.http.get("http://localhost:3000/sender") }
   getRequesterData() { return this.http.get("http://localhost:3000/requester") }
+  getBillerLiteralData() { return this.http.get("http://localhost:3000/billerliterals") } 
+  getSenderLiteralData() { return this.http.get("http://localhost:3000/senderliterals") }
+   getRequesterLiteralData() { return this.http.get("http://localhost:3000/requesterliterals") }
 }
