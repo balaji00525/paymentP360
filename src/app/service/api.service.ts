3c2cc7f68e6c01143712fcb3b2113e7c13a24b3e
moni15sha <monishasridhar15@gmail.com>
2023-02-10 12:12:02 +0530

    alignment fixed

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -10,5 +10,4 @@ export class ApiService {
   getBillerData() { return this.http.get("http://localhost:3000/biller") }
   getSenderData() { return this.http.get("http://localhost:3000/sender") }
   getRequesterData() { return this.http.get("http://localhost:3000/requester") }
-  getOtpData() { return this.http.get("http://localhost:3000/security-code") }
 }
