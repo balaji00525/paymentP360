873127926f00f0c8bab5761212ed3d5e06ae0e8e
balaji00525 <balajiminnal1@gmail.com>
2023-02-18 10:56:14 +0530

    alignment

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../service/data.service';
 import { Router } from '@angular/router';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+ 
 
   constructor(
     private service: DataService,
-    private router: Router
-  ) { }
+    private router: Router) { }
+
+
+
 
   ngOnInit(): void {
   }
@@ -21,10 +26,10 @@ export class HomeComponent implements OnInit {
 
     this.service.user = val;
     this.router.navigate(['/amounttopay']);
-
-
   }
-
-
-
+ 
 }
+
+
+
+
